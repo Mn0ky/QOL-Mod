2eb3f08a260ee773480c3a2b617961474a3fe0cf
Mn0ky <67206766+Mn0ky@users.noreply.github.com>
2021-10-24 12:04:22 -0400

    Add project files.

---
+++ Patches/ChatManagerPatches.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HarmonyLib;
+using TMPro;
+
+namespace QOL
+{
+    public class ChatManagerPatches
+    {
+        public static void Patches(Harmony harmonyInstance) // ChatManager methods to patch with the harmony instance
+        {
+            var SendChatMessageMethod = AccessTools.Method(typeof(ChatManager), "SendChatMessage");
+            var SendChatMessageMethodPrefix = new HarmonyMethod(typeof(ChatManagerPatches).GetMethod(nameof(ChatManagerPatches.SendChatMessageMethodPrefix))); // Patches SendChatMessage with prefix method
+            harmonyInstance.Patch(SendChatMessageMethod, prefix: SendChatMessageMethodPrefix);
+        }
+
+        public static bool SendChatMessageMethodPrefix(ref string message, ChatManager __instance) // Prefix method for patching the original (SendChatMessageMethod)
+        {
+            if (message.StartsWith("/"))
+            {
+                ChatManagerPatches.Commands(message, __instance);
+                return false;
+            }
+            return true;
+        }
+
+        public static void Commands(string message, ChatManager __instance)
+        {
+            NetworkPlayer localNetworkPlayer = Traverse.Create(__instance).Field("m_NetworkPlayer").GetValue() as NetworkPlayer; // For accessing private variable m_NetworkPlayer in ChatManager
+            string text = message.ToLower();
+            text = text.TrimStart(new char[] { '/' });
+
+            if (text.Contains("shrug")) // Adds shrug emoticon to end of chat message
+            {
+                message = message.Replace("/shrug", "");
+                message += " \u00af\\_(ツ)_/\u00af";
+                localNetworkPlayer.OnTalked(message);
+                return;
+            }
+
+            if (text == "rich") // Enables rich text for chat messages
+            {
+                TextMeshPro theText = Traverse.Create(__instance).Field("text").GetValue() as TextMeshPro;
+                theText.richText = !theText.richText;
+                return;
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using BepInEx;
+using HarmonyLib;   
+
+namespace QOL
+{
+    [BepInPlugin("monky.plugins.QOL", "QOL Mod", "1.0.5")]
+    [BepInProcess("StickFight.exe")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo("Plugin 'monky.plugins.QOL' is loaded!");
+            Logger.LogInfo("Hello from monk :D");
+            try
+            {
+                Harmony harmony = new Harmony("monky.QOL"); // Creates harmony instance with identifier
+                ChatManagerPatches.Patches(harmony);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogInfo("Exception on applying patches: " + ex.Message);
+            }
+        }
+    }
+}
