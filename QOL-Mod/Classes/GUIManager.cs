0b86c9ab534349b4de4152071e9e150f6ef0a8e0
Mn0ky <67206766+Mn0ky@users.noreply.github.com>
2021-10-24 21:47:45 -0400

    Implemented GUIManager.

---
+++ QOL-Mod/Classes/GUIManager.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Steamworks;
+
+namespace QOL
+{
+    public class GUIManager : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("Started GUI or called class?");
+            this.mTestNetworkPlayer = base.gameObject.GetComponent<NetworkPlayer>();
+        }
+        private void Awake()
+        {
+        }
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.F1) && !ChatManager.isTyping)
+            {
+                this.mShowMenu = !this.mShowMenu;
+            }
+        }
+        public void OnGUI()
+        {
+            if (!this.mShowMenu)
+            {
+                return;
+            }
+            this.MenuRect = GUILayout.Window(this.WindowId, this.MenuRect, new GUI.WindowFunction(this.KickWindow), "<color=red><b><i>Monk's QOL Menu</i></b></color>\t[v1.0.5]", new GUILayoutOption[0]);
+        }
+		private void KickWindow(int window)
+		{
+			GUILayout.Label("\t<color=#228f69>Show / Hide Menu (Q)</color>", new GUILayoutOption[0]);
+			GUILayout.Label("<color=red>Lobby ID:</color> " + MatchmakingHandlerPatch.lobbyID, new GUILayoutOption[0]);
+			GUILayout.Label("Host: " + GUIManager.GetPlayerName(this.mMatchmaking.LobbyOwner), new GUILayoutOption[0]);
+			string text = "Players in Room: \n";
+			foreach (NetworkPlayer networkPlayer in UnityEngine.Object.FindObjectsOfType<NetworkPlayer>())
+			{
+				string str = string.Concat(new object[]
+				{
+				"[",
+				this.GetColor(networkPlayer.NetworkSpawnID),
+				"] ",
+				GUIManager.GetPlayerName(ChatManagerPatches.GetSteamID(networkPlayer.NetworkSpawnID))
+				});
+				text = text + "\n" + str;
+			}
+			GUILayout.Label(text, new GUILayoutOption[0]);
+			if (GUI.Button(new Rect(120f, 335f, 100f, 30f), "Get Lobby Link"))
+			{
+				ChatManagerPatches.GetJoinGameLink(MatchmakingHandlerPatch.lobbyID, ChatManagerPatches.localPlayerSteamID);
+			}
+		}
+        public string GetColor(ushort x)
+        {
+            switch (x)
+            {
+                case 1:
+                    return "Blue";
+                case 2:
+                    return "Red";
+                case 3:
+                    return "Green";
+                default:
+                    return "Yellow";
+            }
+        }
+        public static string GetPlayerName(CSteamID passedClientID)
+        {
+            return SteamFriends.GetFriendPersonaName(passedClientID);
+        }
+        private MultiplayerManager mManager = UnityEngine.Object.FindObjectOfType<MultiplayerManager>();
+        private MatchmakingHandler mMatchmaking = UnityEngine.Object.FindObjectOfType<MatchmakingHandler>();
+        private NetworkPlayer mTestNetworkPlayer;
+        private bool mShowMenu;
+        private Rect MenuRect = new Rect(0f, 100f, 350f, 375f);
+        private int WindowId = 100;
+    }
+}
--- QOL-Mod/Patches/ChatManagerPatches.cs
+++ QOL-Mod/Patches/ChatManagerPatches.cs
@@ -13,11 +13,28 @@ namespace QOL
     {
         public static void Patches(Harmony harmonyInstance) // ChatManager methods to patch with the harmony instance
         {
+            var AwakeMethod = AccessTools.Method(typeof(ChatManager), "Awake");
+            var AwakeMethodPostfix = new HarmonyMethod(typeof(ChatManagerPatches).GetMethod(nameof(ChatManagerPatches.AwakeMethodPostfix))); // Patches Awake with prefix method
+            harmonyInstance.Patch(AwakeMethod, postfix: AwakeMethodPostfix);
+
+            var StartMethod = AccessTools.Method(typeof(ChatManager), "Start");
+            var StartMethodPostfix = new HarmonyMethod(typeof(ChatManagerPatches).GetMethod(nameof(ChatManagerPatches.StartMethodPostfix))); // Patches Awake with prefix method
+            harmonyInstance.Patch(StartMethod, postfix: StartMethodPostfix);
+
             var SendChatMessageMethod = AccessTools.Method(typeof(ChatManager), "SendChatMessage");
             var SendChatMessageMethodPrefix = new HarmonyMethod(typeof(ChatManagerPatches).GetMethod(nameof(ChatManagerPatches.SendChatMessageMethodPrefix))); // Patches SendChatMessage with prefix method
             harmonyInstance.Patch(SendChatMessageMethod, prefix: SendChatMessageMethodPrefix);
+    }
+        public static void AwakeMethodPostfix(ChatManager __instance)
+        {
+            __instance.gameObject.transform.root.gameObject.AddComponent<GUIManager>();
+            Debug.Log("Added GUIManager!");
         }
-
+        public static void StartMethodPostfix(ChatManager __instance)
+        {
+            NetworkPlayer localNetworkPlayer = Traverse.Create(__instance).Field("m_NetworkPlayer").GetValue() as NetworkPlayer;
+            ChatManagerPatches.localPlayerSteamID = ChatManagerPatches.GetSteamID(localNetworkPlayer.NetworkSpawnID);
+        }
         public static bool SendChatMessageMethodPrefix(ref string message, ChatManager __instance) // Prefix method for patching the original (SendChatMessageMethod)
         {
             if (message.StartsWith("/"))
@@ -31,27 +48,10 @@ namespace QOL
         public static void Commands(string message, ChatManager __instance)
         {
             Debug.Log("Made it to beginning of commands!");
-            MatchmakingHandler matchmaking = Traverse.Create(UnityEngine.Object.FindObjectOfType<MultiplayerManager>()).Field("mMatchmakingHandler").GetValue() as MatchmakingHandler; // For accessing private variable mServerID in ChatManager (this is the host's steamID)
-            CSteamID serverID =  matchmaking.LobbyOwner;
-            Debug.Log("Made it passed serverID assignment! : " + serverID);
             NetworkPlayer localNetworkPlayer = Traverse.Create(__instance).Field("m_NetworkPlayer").GetValue() as NetworkPlayer; // For accessing private variable m_NetworkPlayer in ChatManager
             string text = message.ToLower();
             text = text.TrimStart(new char[] { '/' });
 
-            if (text.Contains("shrug")) // Adds shrug emoticon to end of chat message
-            {
-                message = message.Replace("/shrug", "");
-                message += " \u00af\\_(ツ)_/\u00af";
-                localNetworkPlayer.OnTalked(message);
-                return;
-            }
-
-            if (text == "rich") // Enables rich text for chat messages
-            {
-                TextMeshPro theText = Traverse.Create(__instance).Field("text").GetValue() as TextMeshPro;
-                theText.richText = !theText.richText;
-                return;
-            }
             if (text.Contains("hp") && localNetworkPlayer.HasLocalControl) // Sends HP of targeted color to chat
             {
                 if (text.Length > 2)
@@ -67,17 +67,32 @@ namespace QOL
                 localNetworkPlayer.OnTalked("My HP: " + localHealth);
                 return;
             }
-            if (text == "private") // Privates the lobby (no player can publicly join unless invited)
+
+            else if (text.Contains("shrug")) // Adds shrug emoticon to end of chat message
+            {
+                message = message.Replace("/shrug", "");
+                message += " \u00af\\_(ツ)_/\u00af";
+                localNetworkPlayer.OnTalked(message);
+                return;
+            }
+
+            else if (text == "rich") // Enables rich text for chat messages
+            {
+                TextMeshPro theText = Traverse.Create(__instance).Field("text").GetValue() as TextMeshPro;
+                theText.richText = !theText.richText;
+                return;
+            }
+            else if (text == "private") // Privates the lobby (no player can publicly join unless invited)
             {
                 SteamMatchmaking.SetLobbyJoinable(MatchmakingHandlerPatch.lobbyID, false);
                 localNetworkPlayer.OnTalked("Lobby is now private!");
             }
-            if (text == "public") // Publicizes the lobby (any player can join through quick match)
+            else if (text == "public") // Publicizes the lobby (any player can join through quick match)
             {
                 SteamMatchmaking.SetLobbyJoinable(MatchmakingHandlerPatch.lobbyID, true);
                 localNetworkPlayer.OnTalked("Lobby is now public!");
             }
-            if (text == "invite") // Builds a "join game" link (same one you'd find on a steam profile) for lobby and copies it to clipboard
+            else if (text == "invite") // Builds a "join game" link (same one you'd find on a steam profile) for lobby and copies it to clipboard
             {
                 Debug.Log("LobbyID: " + MatchmakingHandlerPatch.lobbyID);
                 Debug.Log("Verification test, should return 25: " + SteamMatchmaking.GetLobbyData(MatchmakingHandlerPatch.lobbyID, StickFightConstants.VERSION_KEY));
@@ -130,6 +145,6 @@ namespace QOL
             ConnectedClientData[] connectedClients = Traverse.Create(UnityEngine.Object.FindObjectOfType<MultiplayerManager>()).Field("mConnectedClients").GetValue() as ConnectedClientData[];
             return connectedClients[(int)targetID].ClientID;
         }
-
+        public static CSteamID localPlayerSteamID;
     }
 }
