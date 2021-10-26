11e831b3a9fae53a52de8b8a3e6914907d020a87
Mn0ky <67206766+Mn0ky@users.noreply.github.com>
2021-10-26 09:06:11 -0400

    Restructered files.

---
+++ QOL-Mod/Additional Classes/Helper.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Steamworks;
+
+namespace QOL
+{
+    public class Helper
+    {
+        Debug.Log("test");
+    }
+}
