9bad382fdb05c86f38c82f5ac5b841f6636519c5
Mn0ky <67206766+Mn0ky@users.noreply.github.com>
2021-11-02 22:37:37 -0400

    Uparrow system now works!

--- QOL-Mod/Patches/ChatManagerPatches.cs
+++ QOL-Mod/Patches/ChatManagerPatches.cs
@@ -4,7 +4,6 @@ using System.Reflection.Emit;
 using System.Collections.Generic;
 using System.Linq;
 using HarmonyLib;
-using JetBrains.Annotations;
 using TMPro;
 using UnityEngine;
 using Steamworks;
@@ -16,10 +15,6 @@ namespace QOL
     {
         public static void Patches(Harmony harmonyInstance) // ChatManager methods to patch with the harmony instance
         {
-            var AwakeMethod = AccessTools.Method(typeof(ChatManager), "Awake");
-            var AwakeMethodPostfix = new HarmonyMethod(typeof(ChatManagerPatches).GetMethod(nameof(ChatManagerPatches.AwakeMethodPostfix))); // Patches Awake with prefix method
-            harmonyInstance.Patch(AwakeMethod, postfix: AwakeMethodPostfix);
-
             var StartMethod = AccessTools.Method(typeof(ChatManager), "Start");
             var StartMethodPostfix = new HarmonyMethod(typeof(ChatManagerPatches).GetMethod(nameof(ChatManagerPatches.StartMethodPostfix))); // Patches Awake with prefix method
             harmonyInstance.Patch(StartMethod, postfix: StartMethodPostfix);
@@ -40,22 +35,20 @@ namespace QOL
             var ReplaceUnacceptableWordsMethodPrefix = new HarmonyMethod(typeof(ChatManagerPatches).GetMethod(nameof(ChatManagerPatches.ReplaceUnacceptableWordsMethodPrefix))); // Patches SendChatMessage with prefix method
             harmonyInstance.Patch(ReplaceUnacceptableWordsMethod, prefix: ReplaceUnacceptableWordsMethodPrefix);
         }
-        public static void AwakeMethodPostfix(ChatManager __instance)
-        {
-            // __instance.gameObject.transform.root.gameObject.AddComponent<GUIManager>();
-            // Debug.Log("Awake method skipped!");
-        }
+
         public static void StartMethodPostfix(ChatManager __instance)
         {
             NetworkPlayer localNetworkPlayer = Traverse.Create(__instance).Field("m_NetworkPlayer").GetValue() as NetworkPlayer;
             Helper.AssignLocalNetworkPlayer(localNetworkPlayer);
 
         }
+
         public static IEnumerable<CodeInstruction> UpdateMethodTranspiler(IEnumerable<CodeInstruction> instructions, ILGenerator ilGen)
         {
 
             var StopTypingMethod = typeof(ChatManager).GetMethod("StopTyping", BindingFlags.Instance | BindingFlags.NonPublic);
-            var DebugLogMethod = typeof(Debug).GetMethod(nameof(Debug.Log), new[] { typeof(object) });
+            var CheckForArrowKeysMethod = typeof(ChatManagerPatches).GetMethod(nameof(CheckForArrowKeys), BindingFlags.Static | BindingFlags.Public);
+            var DebugLogMethod = typeof(Debug).GetMethod(nameof(Debug.Log), new[] {typeof(object)});
             var list = instructions.ToList();
             var len = list.Count;
             for (var i = 0; i < len; i++)
@@ -77,7 +70,7 @@ namespace QOL
                     CodeInstruction instruction2 = new CodeInstruction(OpCodes.Ldfld, list[9].operand);
                     list.Insert(21, instruction2);
 
-                    CodeInstruction instruction3 = new CodeInstruction(OpCodes.Call, SymbolExtensions.GetMethodInfo(() => CheckForArrowKeys(null)));
+                    CodeInstruction instruction3 = new CodeInstruction(OpCodes.Call, CheckForArrowKeysMethod);
                     list.Insert(22, instruction3);
                     break;
                 }
@@ -85,11 +78,12 @@ namespace QOL
 
             for (var i = 0; i < len; i++)
             {
-                Debug.Log(i + "\t" + list[i]); 
+                Debug.Log(i + "\t" + list[i]);
             }
 
             return list.AsEnumerable();
         }
+
         public static void StopTypingMethodPostfix()
         {
             Debug.Log("ChatManagerPatches.upArrowCounter : " + ChatManagerPatches.upArrowCounter);
@@ -103,6 +97,7 @@ namespace QOL
                 ChatManagerPatches.Commands(message, __instance);
                 return false;
             }
+
             return true;
         }
         public static bool ReplaceUnacceptableWordsMethodPrefix(ref string message, ref string __result) // Prefix method for patching the original (ReplaceUnacceptableWordsMethod)
@@ -113,9 +108,11 @@ namespace QOL
                 __result = message;
                 return false;
             }
+
             Debug.Log("censoring message");
             return true;
         }
+
         public static void Commands(string message, ChatManager __instance)
         {
             Debug.Log("Made it to beginning of commands!");
@@ -140,7 +137,7 @@ namespace QOL
                 string localHealth = Helper.localNetworkPlayer.GetComponentInChildren<HealthHandler>().health.ToString() + "%";
                 Debug.Log("Current Health: " + localHealth);
                 Helper.localNetworkPlayer.OnTalked("My HP: " + localHealth);
-                
+
             }
             else if (text == "gg") // Enables or disables automatic "gg" upon death
             {
@@ -156,13 +153,13 @@ namespace QOL
                 message = message.Replace("/shrug", "");
                 message += " \u00af\\_(ツ)_/\u00af";
                 Helper.localNetworkPlayer.OnTalked(message);
-                
+
             }
             else if (text == "rich") // Enables rich text for chat messages
             {
                 TextMeshPro theText = Traverse.Create(__instance).Field("text").GetValue() as TextMeshPro;
                 theText.richText = !theText.richText;
-                
+
             }
             else if (text == "private") // Privates the lobby (no player can publicly join unless invited)
             {
@@ -178,21 +175,18 @@ namespace QOL
                 text == "invite") // Builds a "join game" link (same one you'd find on a steam profile) for lobby and copies it to clipboard
             {
                 Debug.Log("LobbyID: " + Helper.lobbyID);
-                Debug.Log("Verification test, should return 25: " +
-                          SteamMatchmaking.GetLobbyData(Helper.lobbyID, StickFightConstants.VERSION_KEY));
+                Debug.Log("Verification test, should return 25: " + SteamMatchmaking.GetLobbyData(Helper.lobbyID, StickFightConstants.VERSION_KEY));
                 Helper.GetJoinGameLink();
                 Helper.localNetworkPlayer.OnTalked("Join link copied to clipboard!");
             }
             else if (text == "translate") // Whether or not to enable automatic translations
             {
                 Helper.isTranslating = !Helper.isTranslating;
-                //__instance.StartCoroutine(Translate.Process("en", "Bonjour.", delegate (string s) { Helper.localNetworkPlayer.OnTalked(s); }));
             }
         }
+            
         public static void CheckForArrowKeys(TMP_InputField chatField)
         {
-            Debug.Log("chatField.text : " + chatField.text);
-            Debug.Log("backupTextList : " + ChatManagerPatches.backupTextList);
             if (Input.GetKeyDown(KeyCode.UpArrow) && ChatManagerPatches.upArrowCounter < ChatManagerPatches.backupTextList.Count)
             {
                 Debug.Log("UpArrow, current: " + ChatManagerPatches.upArrowCounter);
@@ -200,6 +194,7 @@ namespace QOL
                 ChatManagerPatches.upArrowCounter++;
                 Debug.Log("UpArrow, now: " + ChatManagerPatches.upArrowCounter);
             }
+
             if (Input.GetKeyDown(KeyCode.DownArrow) && ChatManagerPatches.upArrowCounter > 0)
             {
                 Debug.Log("DownArrow, current: " + ChatManagerPatches.upArrowCounter);
@@ -208,23 +203,35 @@ namespace QOL
                 Debug.Log("DownArrow, now: " + ChatManagerPatches.upArrowCounter);
             }
         }
+
         public static void SaveForUpArrow(string backupThisText)
         {
-            if (!ChatManagerPatches.backupTextList.Any<string>() &&  backupThisText.Length <= 350)
+            Debug.Log("backupThisText : " + backupThisText);
+            Debug.Log("backupTextList[0] : " + backupTextList[0]);
+            if (!ChatManagerPatches.backupTextList.Any<string>() && backupThisText.Length <= 350)
             {
-                ChatManagerPatches.backupTextList.Insert(0, backupThisText);
+                Debug.Log("Attempting to insert");
+                ChatManagerPatches.backupTextList.Add(backupThisText);
+                ChatManagerPatches.backupTextList.RemoveAt(0);
             }
+
             if (ChatManagerPatches.backupTextList.Count == 20)
             {
                 ChatManagerPatches.backupTextList.RemoveAt(19);
             }
+
             if (ChatManagerPatches.backupTextList[0] != backupThisText && backupThisText.Length <= 350)
             {
                 ChatManagerPatches.backupTextList.Insert(0, backupThisText);
             }
         }
 
-        private static List<string> backupTextList;
-        private static int upArrowCounter;
+        public static int upArrowCounter;
+
+        public static List<string> backupTextList = new()
+        {
+
+            string.Empty
+        };
     }
 }
